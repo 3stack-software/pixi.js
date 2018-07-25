79f178b7d61d3194dc8a3e6ede76331a75317e9e
Jackson <jackson.powell@3stack.com.au>
2018-07-25 17:10:34 +1000

    Test merging the source from PIXI and online.
    Adding `Efficient WebGL stroking` concepts and code
    to the original PIXI `buildLine` source.
    
    The way the source is constructed with PIXI is very
    different to the online code and this didnt work out.

--- src/core/graphics/webgl/utils/buildLine.js
+++ src/core/graphics/webgl/utils/buildLine.js
@@ -36,6 +36,7 @@ export default function (graphicsData, webGLData, webGLDataNativeLines)
  */
 function buildLine(graphicsData, webGLData)
 {
+    console.log('building a line');
     // TODO OPTIMISE!
     let points = graphicsData.points;
 
@@ -75,7 +76,7 @@ function buildLine(graphicsData, webGLData)
         points.push(midPointX, midPointY);
     }
 
-    const verts = webGLData.points;
+    let verts = webGLData.points;
     const indices = webGLData.indices;
     const length = points.length / 2;
     let indexCount = points.length;
@@ -91,6 +92,7 @@ function buildLine(graphicsData, webGLData)
     const g = color[1] * alpha;
     const b = color[2] * alpha;
 
+    // FIRST TRIANGLE
     let p1x = points[0];
     let p1y = points[1];
     let p2x = points[2];
@@ -112,25 +114,25 @@ function buildLine(graphicsData, webGLData)
     perpx *= width;
     perpy *= width;
 
-    const ratio = graphicsData.lineAlignment;// 0.5;
-    const r1 = (1 - ratio) * 2;
-    const r2 = ratio * 2;
+    // const ratio = graphicsData.lineAlignment;// 0.5;
+    // const r1 = (1 - ratio) * 2;
+    // const r2 = ratio * 2;
 
     // start
     verts.push(
-        p1x - (perpx * r1),
-        p1y - (perpy * r1),
+        p1x - perpx,
+        p1y - perpy,
         r, g, b, alpha
     );
 
     verts.push(
-        p1x + (perpx * r2),
-        p1y + (perpy * r2),
+        p1x + perpx,
+        p1y + perpy,
         r, g, b, alpha
     );
 
     for (let i = 1; i < length - 1; ++i)
-    {
+     {
         p1x = points[(i - 1) * 2];
         p1y = points[((i - 1) * 2) + 1];
 
@@ -171,14 +173,14 @@ function buildLine(graphicsData, webGLData)
         {
             denom += 10.1;
             verts.push(
-                p2x - (perpx * r1),
-                p2y - (perpy * r1),
+                p2x - (perpx),
+                p2y - (perpy),
                 r, g, b, alpha
             );
 
             verts.push(
-                p2x + (perpx * r2),
-                p2y + (perpy * r2),
+                p2x + (perpx),
+                p2y + (perpy),
                 r, g, b, alpha
             );
 
@@ -189,7 +191,8 @@ function buildLine(graphicsData, webGLData)
         const py = ((a2 * c1) - (a1 * c2)) / denom;
         const pdist = ((px - p2x) * (px - p2x)) + ((py - p2y) * (py - p2y));
 
-        if (pdist > (196 * width * width))
+        // TODO JP: can set mitre limit here, might break other things though?
+        if (pdist > (15 * width * width))
         {
             perp3x = perpx - perp2x;
             perp3y = perpy - perp2y;
@@ -200,26 +203,39 @@ function buildLine(graphicsData, webGLData)
             perp3x *= width;
             perp3y *= width;
 
-            verts.push(p2x - (perp3x * r1), p2y - (perp3y * r1));
+            const p01 = { x: p2x - (perp3x), y: p2y - (perp3y) };
+            const p02 = { x: p2x + (perp3x), y: p2y + (perp3y) };
+            const p03 = { x: p2x - (perp3x), y: p2y - (perp3y) };
+
+            verts.push(p2x - (perp3x ), p2y - (perp3y));
             verts.push(r, g, b, alpha);
 
-            verts.push(p2x + (perp3x * r2), p2y + (perp3y * r2));
+            verts.push(p2x + (perp3x), p2y + (perp3y));
             verts.push(r, g, b, alpha);
 
-            verts.push(p2x - (perp3x * r2 * r1), p2y - (perp3y * r1));
+            verts.push(p2x - (perp3x), p2y - (perp3y));
             verts.push(r, g, b, alpha);
 
             indexCount++;
+
+            let addedVertCount = createRoundCap(midPoint(p01, p02), p01, p02, p03, verts);
+            console.log(addedVertCount);
+            while(addedVertCount > 0) {
+                indexCount+=5;
+                addedVertCount--;
+            }
         }
         else
         {
-            verts.push(p2x + ((px - p2x) * r1), p2y + ((py - p2y) * r1));
+            verts.push(p2x + ((px - p2x)), p2y + ((py - p2y)));
             verts.push(r, g, b, alpha);
 
-            verts.push(p2x - ((px - p2x) * r2), p2y - ((py - p2y) * r2));
+            verts.push(p2x - ((px - p2x)), p2y - ((py - p2y)));
             verts.push(r, g, b, alpha);
         }
     }
+
+    // LAST TRIANGLE
 
     p1x = points[(length - 2) * 2];
     p1y = points[((length - 2) * 2) + 1];
@@ -236,21 +252,136 @@ function buildLine(graphicsData, webGLData)
     perpx *= width;
     perpy *= width;
 
-    verts.push(p2x - (perpx * r1), p2y - (perpy * r1));
+    verts.push(p2x - (perpx), p2y - (perpy));
     verts.push(r, g, b, alpha);
 
-    verts.push(p2x + (perpx * r2), p2y + (perpy * r2));
+    verts.push(p2x + (perpx), p2y + (perpy));
     verts.push(r, g, b, alpha);
 
     indices.push(indexStart);
+
+    // Add roundcap to the start and end of the line.
+    // const p00 = { x: verts[6], y: verts[6 + 1] };
+    // const p01 = { x: verts[12], y: verts[12 + 1] };
+    // const p02 = { x: points[2], y: points[3] };
+    // let addedVertCount = createRoundCap(
+    //     { x: points[0], y: points[1] }, p00, p01, p02, verts
+    // );
+    // while(addedVertCount > 0) {
+    //     indices.push(indexStart++);
+    //     addedVertCount--;
+    // }
 
     for (let i = 0; i < indexCount; ++i)
     {
         indices.push(indexStart++);
     }
 
+    // TODO TRYING TO ADD END CAP
+
+    // const p10 = { x: verts[verts.length - 6], y: verts[verts.length - 5] };
+    // const p11 = { x: verts[verts.length - 12], y: verts[verts.length - 11] };
+    // const p12 = { x: points[points.length - 4], y: points[points.length - 3] };
+    // let addedVertCount = createRoundCap(
+    //     { x: points[points.length - 2], y: points[points.length - 1] }, p10, p11, p12, verts
+    // );
+    // while(addedVertCount > 0) {
+    //     indices.push(indexStart++);
+    //     addedVertCount--;
+    // }
+
     indices.push(indexStart - 1);
 }
+
+
+export function perpendicular(x, y) {
+    return { x: -y, y: x };
+}
+
+export function invert(x, y) {
+    return { x: -x, y: -y };
+}
+
+export function normalize(x, y) {
+    const mod = toLength(x, y);
+    return { x: x / mod, y: y / mod };
+}
+
+export function toLength(x, y) {
+    return Math.sqrt((x * x) + (y * y));
+}
+
+export function multiplyPoint(x, y, times) {
+    return { x: x * times, y: y * times };
+}
+
+export function midPoint(p1, p2) {
+    const add = addPoints(p1, p2);
+    return multiplyPoint(add.x, add.y, 0.5);
+}
+
+export function addPoints(p1, p2) {
+    return { x: p1.x + p2.x, y: p1.y + p2.y };
+}
+
+export function subtractPoints(p1, p2) {
+    return { x: p1.x - p2.x, y: p1.y - p2.y };
+}
+
+
+const EPSILON = 0.0001;
+export function createRoundCap(center, _p0, _p1, nextPointInLine, vertices) {
+    const outerRadiusPoint = subtractPoints(center, _p0);
+    const radius = toLength(outerRadiusPoint.x, outerRadiusPoint.y);
+
+    let angle0 = Math.atan2((_p1.y - center.y), (_p1.x - center.x));
+    let angle1 = Math.atan2((_p0.y - center.y), (_p0.x - center.x));
+
+    const originalAngle0 = angle0;
+
+    if (angle1 > angle0) {
+        if (angle1 - angle0 >= Math.PI - EPSILON) {
+            angle1 = angle1 - (2 * Math.PI);
+        }
+    } else {
+        if (angle0 - angle1 >= Math.PI - EPSILON) {
+            angle0 = angle0 - (2 * Math.PI);
+        }
+    }
+
+    let angleDiff = angle1 - angle0;
+
+    if (Math.abs(angleDiff) >= Math.PI - EPSILON && Math.abs(angleDiff) <= Math.PI + EPSILON) {
+        let r1 = subtractPoints(center, nextPointInLine);
+        if (r1.x === 0) {
+            if (r1.y > 0) {
+                angleDiff = -angleDiff;
+            }
+        } else if (r1.x >= -EPSILON) {
+            angleDiff = -angleDiff;
+        }
+    }
+
+    const nsegments = Math.min(Math.abs(angleDiff * radius), 10) + 1;
+    const angleInc = angleDiff / nsegments;
+
+    for (let i = 0; i < nsegments; i++) {
+        vertices.push(center.x, center.y);
+        vertices.push(0, 25, 0, 1);
+        vertices.push(
+            center.x + radius * Math.cos(originalAngle0 + angleInc * (i)),
+            center.y + radius * Math.sin(originalAngle0 + angleInc * (i)),
+        );
+        vertices.push(0, 25, 0, 1);
+        vertices.push(
+            center.x + radius * Math.cos(originalAngle0 + angleInc * (i + 1)),
+            center.y + radius * Math.sin(originalAngle0 + angleInc * (i + 1)),
+        );
+        vertices.push(0, 0, 25, 1);
+    }
+    return nsegments;
+}
+
 
 /**
  * Builds a line to draw using the gl.drawArrays(gl.LINES) method
@@ -263,6 +394,53 @@ function buildLine(graphicsData, webGLData)
  * @param {object} webGLData - an object containing all the webGL-specific information to create this shape
  */
 function buildNativeLine(graphicsData, webGLData)
+{
+    let i = 0;
+    const points = graphicsData.points;
+
+    if (points.length === 0) return;
+
+    let verts = webGLData.points;
+    const length = points.length / 2;
+
+    // sort color
+    const color = hex2rgb(graphicsData.lineColor);
+    const alpha = graphicsData.lineAlpha;
+    const r = color[0] * alpha;
+    const g = color[1] * alpha;
+    const b = color[2] * alpha;
+
+    for (i = 1; i < length; i++)
+    {
+        const p1x = points[(i - 1) * 2];
+        const p1y = points[((i - 1) * 2) + 1];
+
+        const p2x = points[i * 2];
+        const p2y = points[(i * 2) + 1];
+
+        const p3x = points[(i + 1) * 2];
+        const p3y = points[((i + 1) * 2) + 1];
+
+        verts.push(p1x, p1y);
+        verts.push(r, g, b, alpha);
+
+        verts.push(p2x, p2y);
+        verts.push(r, g, b, alpha);
+
+        // const p01 = { x: p1x, y: p1y };
+        // const p02 = { x: p2x, y: p2y };
+        // const p03 = { x: p3x, y: p3y };
+        // createRoundCap(midPoint(p01, p02), p01, p02, p03, verts);
+    }
+
+    // const p01 = { x: points[0], y: points[1] };
+    // const p02 = { x: points[2], y: points[3] };
+    // const p03 = { x: points[4], y: points[5] };
+    // createRoundCap(midPoint(p01, p02), p01, p02, p03, verts);
+}
+
+
+function buildNativeLine2(graphicsData, webGLData)
 {
     let i = 0;
     const points = graphicsData.points;
@@ -292,5 +470,184 @@ function buildNativeLine(graphicsData, webGLData)
 
         verts.push(p2x, p2y);
         verts.push(r, g, b, alpha);
+    }
+}
+
+export function pointsArrayToObjects(points) {
+    const pointObjects = [];
+    for (let i = 0; i < points.length; i += 2) {
+        pointObjects.push({
+            x: points[i],
+            y: points[i + 1],
+        });
+    }
+    return pointObjects;
+}
+
+export function createLineFromPoints(points) {
+    if (points.length < 2) {
+        return;
+    }
+
+    let radius = 3;
+    let vertices = [];
+    let middlePoints = [];  // middle points per each line segment.
+
+    if (points.length === 2) {
+        // Straight line
+        // createTriangles(points[0], midPoint(points[0], points[1]), points[1], radius, vertices);
+    } else {
+        if (points[0] === points[points.length - 1] ||
+            (points[0].x === points[points.length - 1].x && points[0].y === points[points.length - 1].y)) {
+            let p0 = points.shift();
+            p0 = midPoint(p0, points[0]);
+            points.unshift(p0);
+            points.push(p0);
+        }
+        let i;
+        for (i = 0; i < points.length - 1; i++) {
+            if (i === 0) {
+                middlePoints.push(points[0]);
+            } else if (i === points.length - 2) {
+                middlePoints.push(points[points.length - 1])
+            } else {
+                middlePoints.push(midPoint(points[i], points[i + 1]));
+            }
+        }
+        for (i = 1; i < middlePoints.length; i++) {
+            // createTriangles(middlePoints[i - 1], points[i], middlePoints[i], radius, vertices);
+        }
+    }
+
+    const p00 = vertices[0];
+    const p01 = vertices[1];
+    const p02 = points[1];
+    const p10 = vertices[vertices.length - 1];
+    const p11 = vertices[vertices.length - 3];
+    const p12 = points[points.length - 2];
+    //
+    // // Add roundcap to the start and end of the line.
+    // createRoundCap(points[0], p00, p01, p02, vertices);
+    // createRoundCap(points[points.length - 1], p10, p11, p12, vertices);
+
+    return vertices;
+}
+
+function signedArea(p0, p1, p2) {
+    return (p1.x - p0.x) * (p2.y - p0.y) - (p2.x - p0.x) * (p1.y - p0.y);
+}
+
+function lineIntersection(p0, p1, p2, p3) {
+    let a0 = p1.y - p0.y;
+    let b0 = p0.x - p1.x;
+
+    let a1 = p3.y - p2.y;
+    let b1 = p2.x - p3.x;
+
+    let det = a0 * b1 - a1 * b0;
+    if (det > -EPSILON && det < EPSILON) {
+        return null;
+    } else {
+        let c0 = a0 * p0.x + b0 * p0.y;
+        let c1 = a1 * p2.x + b1 * p2.y;
+
+        let x = (b1 * c0 - b0 * c1) / det;
+        let y = (a0 * c1 - a1 * c0) / det;
+        return { x, y };
+    }
+}
+
+
+function createTriangles(p0, p1, p2, lineWidth, vertices) {
+    let t0 = subtractPoints(p1, p0);
+    let t2 = subtractPoints(p2, p1);
+
+    t0 = perpendicular(t0.x, t0.y);
+    t2 = perpendicular(t2.x, t2.y);
+
+    // triangle composed by the 3 points if clockwise or couterclockwise.
+    // if counterclockwise, we must invert the line threshold points, otherwise the intersection point
+    // could be erroneous and lead to odd results.
+    if (signedArea(p0, p1, p2) > 0) {
+        t0 = invert(t0.x, t0.y);
+        t2 = invert(t2.x, t2.y);
+    }
+
+    t0 = normalize(t0.x, t0.y);
+    t2 = normalize(t2.x, t2.y);
+    t0 = multiplyPoint(t0.x, t0.y, lineWidth);
+    t2 = multiplyPoint(t2.x, t2.y, lineWidth);
+
+    const pintersect = lineIntersection(addPoints(t0, p0), addPoints(t0, p1), addPoints(t2, p2), addPoints(t2, p1));
+
+    let anchor = null;
+    let anchorLength = Number.MAX_VALUE;
+    if (pintersect) {
+        anchor = subtractPoints(pintersect, p1);
+        anchorLength = toLength(anchor.x, anchor.y);
+    }
+    const dd = (anchorLength / lineWidth) | 0;
+    const p0p1 = subtractPoints(p0, p1);
+    const p0p1Length = toLength(p0p1.x, p0p1.y);
+    const p1p2 = subtractPoints(p1, p2);
+    const p1p2Length = toLength(p1p2.x, p1p2.y);
+
+    /*
+     * The cross point exceeds any of the segments dimension.
+     * Do not use cross point as reference.
+     */
+    if (anchorLength > p0p1Length || anchorLength > p1p2Length) {
+
+        vertices.push(addPoints(p0, t0));
+        vertices.push(subtractPoints(p0, t0));
+        vertices.push(addPoints(p1, t0));
+
+        vertices.push(subtractPoints(p0, t0));
+        vertices.push(addPoints(p1, t0));
+        vertices.push(subtractPoints(p1, t0));
+
+        createRoundCap(p1, addPoints(p1, t0), addPoints(p1, t2), p2, vertices);
+
+        vertices.push(addPoints(p2, t2));
+        vertices.push(subtractPoints(p1, t2));
+        vertices.push(addPoints(p1, t2));
+
+        vertices.push(addPoints(p2, t2));
+        vertices.push(subtractPoints(p1, t2));
+        vertices.push(subtractPoints(p2, t2));
+
+    } else {
+
+        vertices.push(addPoints(p0, t0));
+        vertices.push(subtractPoints(p0, t0));
+        vertices.push(subtractPoints(p1, anchor));
+
+        vertices.push(addPoints(p0, t0));
+        vertices.push(subtractPoints(p1, anchor));
+        vertices.push(addPoints(p1, t0));
+
+        const _p0 = addPoints(p1, t0);
+        const _p1 = addPoints(p1, t2);
+        const _p2 = subtractPoints(p1, anchor);
+        const center = p1;
+
+        vertices.push(_p0);
+        vertices.push(center);
+        vertices.push(_p2);
+
+        createRoundCap(center, _p0, _p1, _p2, vertices);
+
+        vertices.push(center);
+        vertices.push(_p1);
+        vertices.push(_p2);
+
+        vertices.push(addPoints(p2, t2));
+        vertices.push(subtractPoints(p1, anchor));
+        vertices.push(addPoints(p1, t2));
+
+        vertices.push(addPoints(p2, t2));
+        vertices.push(subtractPoints(p1, anchor));
+        vertices.push(subtractPoints(p2, t2));
+
     }
 }
