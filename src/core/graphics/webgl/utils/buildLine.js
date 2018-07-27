1b8ac681740211ccf831851b26cb31a971817500
Jackson <jackson.powell@3stack.com.au>
2018-07-27 16:15:38 +1000

    Testing with createTriangles function.
    Creating triangles within the buildLine function
    and adding round caps to them.
    
    This commit does not work as intended.

--- src/core/graphics/webgl/utils/buildLine.js
+++ src/core/graphics/webgl/utils/buildLine.js
@@ -91,268 +91,104 @@ function buildLine(graphicsData, webGLData)
     const g = color[1] * alpha;
     const b = color[2] * alpha;
 
-    console.log(color[0]);
-    const newMethod = color[0] === 1;
     const vertices = [];
     const middlePoints = [];
 
-    if (newMethod) {
-
-        if (points.length === 2) {
-            // Straight line
-            createTriangles(points[0], midPoint(points[0], points[1]), points[1], radius, vertices);
-        } else {
-            if (points[0] === points[points.length - 2] && points[1] === points[points.length - 1]) {
-                let p0 = points.shift();
-                p0 = midPoint(p0, { x: points[0], y: points[1] });
-                points.unshift(p0);
-                points.push(p0);
-            }
-            let i;
-            for (i = 0; i < points.length; i+=2) {
-                if (i === 0) {
-                    middlePoints.push({ x: points[0], y: points[1] });
-                } else if (i === points.length - 2) {
-                    middlePoints.push({ x: points[points.length - 2], y: points[points.length - 1] });
-                } else {
-                    middlePoints.push(midPoint({ x: points[i], y: points[i+1] }, { x: points[i+2], y: points[i+3] }));
-                }
-            }
-            for (i = 1; i < middlePoints.length; i++) {
-                createTriangles(middlePoints[i - 1], { x: points[i * 2], y: points[i * 2 + 1] }, middlePoints[i], width, vertices);
-            }
-        }
-
-        for (const vertex of vertices) {
-            verts.push(vertex.x, vertex.y);
-            verts.push(r, g, b, 1);
-        }
-
-        // const p00 = verts[0];
-        // const p01 = verts[1];
-        // const p02 = { x: points[2], y: points[3] };
-        // const p10 = verts[verts.length - 1];
-        // const p11 = verts[verts.length - 3];
-        // const p12 = { x: points[points.length - 4], y: points[points.length - 3] };
-        //
-        // // Add roundcap to the start and end of the line.
-        // createRoundCap({ x: points[0], y: points[1] }, p00, p01, p02, verts);
-        // createRoundCap({ x: points[points.length - 2], y: points[points.length - 1] }, p10, p11, p12, verts);
-
-        // indices
-        for (let i = 0; i < vertices.length; ++i)
+    let i;
+    for (i = 0; i < points.length; i += 2)
+    {
+        if (i === 0 || i === points.length - 2)
         {
-            indices.push(indexStart++);
+            middlePoints.push(points[i], points[i + 1]);
         }
-
-    } else {
-
-        let p1x = points[0];
-        let p1y = points[1];
-        let p2x = points[2];
-        let p2y = points[3];
-        let p3x = 0;
-        let p3y = 0;
-
-        let perpx = -(p1y - p2y);
-        let perpy = p1x - p2x;
-        let perp2x = 0;
-        let perp2y = 0;
-        let perp3x = 0;
-        let perp3y = 0;
-
-        let dist = Math.sqrt((perpx * perpx) + (perpy * perpy));
-
-        perpx /= dist;
-        perpy /= dist;
-        perpx *= width;
-        perpy *= width;
-
-        const ratio = graphicsData.lineAlignment;// 0.5;
-        const r1 = (1 - ratio) * 2;
-        const r2 = ratio * 2;
-
-        // start
-        verts.push(
-            p1x - (perpx * r1),
-            p1y - (perpy * r1),
-            r, g, b, alpha
-        );
-
-        verts.push(
-            p1x + (perpx * r2),
-            p1y + (perpy * r2),
-            r, g, b, alpha
-        );
-
-        for (let i = 1; i < length - 1; ++i)
+        else
         {
-            p1x = points[(i - 1) * 2];
-            p1y = points[((i - 1) * 2) + 1];
-
-            p2x = points[i * 2];
-            p2y = points[(i * 2) + 1];
-
-            p3x = points[(i + 1) * 2];
-            p3y = points[((i + 1) * 2) + 1];
-
-            perpx = -(p1y - p2y);
-            perpy = p1x - p2x;
-
-            dist = Math.sqrt((perpx * perpx) + (perpy * perpy));
-            perpx /= dist;
-            perpy /= dist;
-            perpx *= width;
-            perpy *= width;
-
-            perp2x = -(p2y - p3y);
-            perp2y = p2x - p3x;
-
-            dist = Math.sqrt((perp2x * perp2x) + (perp2y * perp2y));
-            perp2x /= dist;
-            perp2y /= dist;
-            perp2x *= width;
-            perp2y *= width;
-
-            const a1 = (-perpy + p1y) - (-perpy + p2y);
-            const b1 = (-perpx + p2x) - (-perpx + p1x);
-            const c1 = ((-perpx + p1x) * (-perpy + p2y)) - ((-perpx + p2x) * (-perpy + p1y));
-            const a2 = (-perp2y + p3y) - (-perp2y + p2y);
-            const b2 = (-perp2x + p2x) - (-perp2x + p3x);
-            const c2 = ((-perp2x + p3x) * (-perp2y + p2y)) - ((-perp2x + p2x) * (-perp2y + p3y));
-
-            let denom = (a1 * b2) - (a2 * b1);
-
-            if (Math.abs(denom) < 0.1)
-            {
-                denom += 10.1;
-                verts.push(
-                    p2x - (perpx * r1),
-                    p2y - (perpy * r1),
-                    r, g, b, alpha
-                );
-
-                verts.push(
-                    p2x + (perpx * r2),
-                    p2y + (perpy * r2),
-                    r, g, b, alpha
-                );
-
-                continue;
-            }
-
-            const px = ((b1 * c2) - (b2 * c1)) / denom;
-            const py = ((a2 * c1) - (a1 * c2)) / denom;
-            const pdist = ((px - p2x) * (px - p2x)) + ((py - p2y) * (py - p2y));
-
-            if (pdist > (196 * width * width))
-            {
-                perp3x = perpx - perp2x;
-                perp3y = perpy - perp2y;
-
-                dist = Math.sqrt((perp3x * perp3x) + (perp3y * perp3y));
-                perp3x /= dist;
-                perp3y /= dist;
-                perp3x *= width;
-                perp3y *= width;
-
-                verts.push(p2x - (perp3x * r1), p2y - (perp3y * r1));
-                verts.push(r, g, b, alpha);
-
-                verts.push(p2x + (perp3x * r2), p2y + (perp3y * r2));
-                verts.push(r, g, b, alpha);
-
-                verts.push(p2x - (perp3x * r2 * r1), p2y - (perp3y * r1));
-                verts.push(r, g, b, alpha);
-
-                indexCount++;
-            }
-            else
-            {
-                verts.push(p2x + ((px - p2x) * r1), p2y + ((py - p2y) * r1));
-                verts.push(r, g, b, alpha);
-
-                verts.push(p2x - ((px - p2x) * r2), p2y - ((py - p2y) * r2));
-                verts.push(r, g, b, alpha);
-            }
+            middlePoints.push(
+                (points[i] + points[i + 2]) * 0.5,
+                (points[i + 1] + points[i + 3]) * 0.5
+            );
         }
-
-        p1x = points[(length - 2) * 2];
-        p1y = points[((length - 2) * 2) + 1];
-
-        p2x = points[(length - 1) * 2];
-        p2y = points[((length - 1) * 2) + 1];
-
-        perpx = -(p1y - p2y);
-        perpy = p1x - p2x;
-
-        dist = Math.sqrt((perpx * perpx) + (perpy * perpy));
-        perpx /= dist;
-        perpy /= dist;
-        perpx *= width;
-        perpy *= width;
-
-        verts.push(p2x - (perpx * r1), p2y - (perpy * r1));
-        verts.push(r, g, b, alpha);
-
-        verts.push(p2x + (perpx * r2), p2y + (perpy * r2));
-        verts.push(r, g, b, alpha);
-
-        indices.push(indexStart);
-
-        for (let i = 0; i < indexCount; ++i)
-        {
-            indices.push(indexStart++);
-        }
-
-        indices.push(indexStart - 1);
+    }
+    for (i = 1; i < middlePoints.length; i++)
+    {
+        createTriangles(middlePoints[i - 1], { x: points[i * 2], y: points[i * 2 + 1] }, middlePoints[i], width, vertices);
     }
 
-    console.log(verts);
-    console.log(indices);
+    for (const vertex of vertices)
+    {
+        verts.push(vertex.x, vertex.y);
+        verts.push(r, g, b, 1);
+    }
+
+    // const p00 = verts[0];
+    // const p01 = verts[1];
+    // const p02 = { x: points[2], y: points[3] };
+    // const p10 = verts[verts.length - 1];
+    // const p11 = verts[verts.length - 3];
+    // const p12 = { x: points[points.length - 4], y: points[points.length - 3] };
+    //
+    // // Add roundcap to the start and end of the line.
+    // createRoundCap({ x: points[0], y: points[1] }, p00, p01, p02, verts);
+    // createRoundCap({ x: points[points.length - 2], y: points[points.length - 1] }, p10, p11, p12, verts);
+
+    // indices
+    for (let i = 0; i < vertices.length; ++i)
+    {
+        indices.push(indexStart++);
+    }
 }
 
-
-export function perpendicular(x, y) {
+export function perpendicular(x, y)
+{
     return { x: -y, y: x };
 }
 
-export function invert(x, y) {
+export function invert(x, y)
+{
     return { x: -x, y: -y };
 }
 
-export function normalize(x, y) {
+export function normalize(x, y)
+{
     const mod = toLength(x, y);
-    if (mod === 0) {
+    if (mod === 0)
+    {
         return { x: 0, y: 0 };
     }
     return { x: x / mod, y: y / mod };
 }
 
-export function toLength(x, y) {
+export function toLength(x, y)
+{
     return Math.sqrt((x * x) + (y * y));
 }
 
-export function multiplyPoint(x, y, times) {
+export function multiplyPoint(x, y, times)
+{
     return { x: x * times, y: y * times };
 }
 
-export function midPoint(p1, p2) {
-    const add = addPoints(p1, p2);
+export function midPoint(p1x, p1y, p2x, p2y)
+{
+    const add = addPoints(p1x, p1y, p2x, p2y);
     return multiplyPoint(add.x, add.y, 0.5);
 }
 
-export function addPoints(p1, p2) {
+export function addPoints(p1, p2)
+{
     return { x: p1.x + p2.x, y: p1.y + p2.y };
 }
 
-export function subtractPoints(p1, p2) {
+export function subtractPoints(p1, p2)
+{
     return { x: p1.x - p2.x, y: p1.y - p2.y };
 }
 
-export function pointsObjectsToArray(points) {
+export function pointsObjectsToArray(points)
+{
     const pointObjects = [];
-    for (const point of points) {
+    for (const point of points)
+    {
         pointObjects.push(point.x);
         pointObjects.push(point.y);
     }
@@ -361,7 +197,8 @@ export function pointsObjectsToArray(points) {
 
 const EPSILON = 0.0001;
 
-export function createRoundCap(center, _p0, _p1, nextPointInLine, vertices) {
+export function createRoundCap(center, _p0, _p1, nextPointInLine, vertices)
+{
     const outerRadiusPoint = subtractPoints(center, _p0);
     const radius = toLength(outerRadiusPoint.x, outerRadiusPoint.y);
 
@@ -370,25 +207,35 @@ export function createRoundCap(center, _p0, _p1, nextPointInLine, vertices) {
 
     const originalAngle0 = angle0;
 
-    if (angle1 > angle0) {
-        if (angle1 - angle0 >= Math.PI - EPSILON) {
+    if (angle1 > angle0)
+    {
+        if (angle1 - angle0 >= Math.PI - EPSILON)
+        {
             angle1 = angle1 - (2 * Math.PI);
         }
-    } else {
-        if (angle0 - angle1 >= Math.PI - EPSILON) {
+    }
+    else
+    {
+        if (angle0 - angle1 >= Math.PI - EPSILON)
+        {
             angle0 = angle0 - (2 * Math.PI);
         }
     }
 
     let angleDiff = angle1 - angle0;
 
-    if (Math.abs(angleDiff) >= Math.PI - EPSILON && Math.abs(angleDiff) <= Math.PI + EPSILON) {
+    if (Math.abs(angleDiff) >= Math.PI - EPSILON && Math.abs(angleDiff) <= Math.PI + EPSILON)
+    {
         let r1 = subtractPoints(center, nextPointInLine);
-        if (r1.x === 0) {
-            if (r1.y > 0) {
+        if (r1.x === 0)
+        {
+            if (r1.y > 0)
+            {
                 angleDiff = -angleDiff;
             }
-        } else if (r1.x >= -EPSILON) {
+        }
+        else if (r1.x >= -EPSILON)
+        {
             angleDiff = -angleDiff;
         }
     }
@@ -396,8 +243,9 @@ export function createRoundCap(center, _p0, _p1, nextPointInLine, vertices) {
     const nsegments = Math.min(Math.abs(angleDiff * radius), 10) + 1;
     const angleInc = angleDiff / nsegments;
 
-    for (let i = 0; i < nsegments; i++) {
-        vertices.push({x: center.x, y: center.y});
+    for (let i = 0; i < nsegments; i++)
+    {
+        vertices.push({ x: center.x, y: center.y });
         vertices.push({
             x: center.x + radius * Math.cos(originalAngle0 + angleInc * i),
             y: center.y + radius * Math.sin(originalAngle0 + angleInc * i),
@@ -407,6 +255,67 @@ export function createRoundCap(center, _p0, _p1, nextPointInLine, vertices) {
             y: center.y + radius * Math.sin(originalAngle0 + angleInc * (i + 1)),
         });
     }
+}
+
+export function createRoundCap2(center, _p0, _p1, nextPointInLine, vertices)
+{
+    const outerRadiusPoint = subtractPoints(center, _p0);
+    const radius = toLength(outerRadiusPoint.x, outerRadiusPoint.y);
+
+    let angle0 = Math.atan2((_p1.y - center.y), (_p1.x - center.x));
+    let angle1 = Math.atan2((_p0.y - center.y), (_p0.x - center.x));
+
+    const originalAngle0 = angle0;
+
+    if (angle1 > angle0)
+    {
+        if (angle1 - angle0 >= Math.PI - EPSILON)
+        {
+            angle1 = angle1 - (2 * Math.PI);
+        }
+    }
+    else
+    {
+        if (angle0 - angle1 >= Math.PI - EPSILON)
+        {
+            angle0 = angle0 - (2 * Math.PI);
+        }
+    }
+
+    let angleDiff = angle1 - angle0;
+
+    if (Math.abs(angleDiff) >= Math.PI - EPSILON && Math.abs(angleDiff) <= Math.PI + EPSILON)
+    {
+        let r1 = subtractPoints(center, nextPointInLine);
+        if (r1.x === 0)
+        {
+            if (r1.y > 0)
+            {
+                angleDiff = -angleDiff;
+            }
+        }
+        else if (r1.x >= -EPSILON)
+        {
+            angleDiff = -angleDiff;
+        }
+    }
+
+    const nsegments = Math.min(Math.abs(angleDiff * radius), 10) + 1;
+    const angleInc = angleDiff / nsegments;
+
+    for (let i = 0; i < nsegments; i++)
+    {
+        vertices.push({ x: center.x, y: center.y });
+        vertices.push({
+            x: center.x + radius * Math.cos(originalAngle0 + angleInc * i),
+            y: center.y + radius * Math.sin(originalAngle0 + angleInc * i),
+        });
+        vertices.push({
+            x: center.x + radius * Math.cos(originalAngle0 + angleInc * (i + 1)),
+            y: center.y + radius * Math.sin(originalAngle0 + angleInc * (i + 1)),
+        });
+    }
+    return nsegments;
 }
 
 /**
@@ -455,11 +364,13 @@ function buildNativeLine(graphicsData, webGLData)
     }
 }
 
-function signedArea(p0, p1, p2) {
+function signedArea(p0, p1, p2)
+{
     return (p1.x - p0.x) * (p2.y - p0.y) - (p2.x - p0.x) * (p1.y - p0.y);
 }
 
-function lineIntersection(p0, p1, p2, p3) {
+function lineIntersection(p0, p1, p2, p3)
+{
     let a0 = p1.y - p0.y;
     let b0 = p0.x - p1.x;
 
@@ -467,9 +378,12 @@ function lineIntersection(p0, p1, p2, p3) {
     let b1 = p2.x - p3.x;
 
     let det = a0 * b1 - a1 * b0;
-    if (det > -EPSILON && det < EPSILON) {
+    if (det > -EPSILON && det < EPSILON)
+    {
         return null;
-    } else {
+    }
+    else
+    {
         let c0 = a0 * p0.x + b0 * p0.y;
         let c1 = a1 * p2.x + b1 * p2.y;
 
@@ -479,7 +393,8 @@ function lineIntersection(p0, p1, p2, p3) {
     }
 }
 
-function createTriangles(p0, p1, p2, lineWidth, vertices) {
+function createTriangles(p0, p1, p2, lineWidth, vertices)
+{
     let t0 = subtractPoints(p1, p0);
     let t2 = subtractPoints(p2, p1);
 
@@ -489,7 +404,8 @@ function createTriangles(p0, p1, p2, lineWidth, vertices) {
     // triangle composed by the 3 points if clockwise or couterclockwise.
     // if counterclockwise, we must invert the line threshold points, otherwise the intersection point
     // could be erroneous and lead to odd results.
-    if (signedArea(p0, p1, p2) > 0) {
+    if (signedArea(p0, p1, p2) > 0)
+    {
         t0 = invert(t0.x, t0.y);
         t2 = invert(t2.x, t2.y);
     }
@@ -503,7 +419,8 @@ function createTriangles(p0, p1, p2, lineWidth, vertices) {
 
     let anchor = null;
     let anchorLength = Number.MAX_VALUE;
-    if (pintersect) {
+    if (pintersect)
+    {
         anchor = subtractPoints(pintersect, p1);
         anchorLength = toLength(anchor.x, anchor.y);
     }
@@ -517,7 +434,8 @@ function createTriangles(p0, p1, p2, lineWidth, vertices) {
      * The cross point exceeds any of the segments dimension.
      * Do not use cross point as reference.
      */
-    if (anchorLength > p0p1Length || anchorLength > p1p2Length) {
+    if (anchorLength > p0p1Length || anchorLength > p1p2Length)
+    {
 
         vertices.push(addPoints(p0, t0));
         vertices.push(subtractPoints(p0, t0));
@@ -537,7 +455,9 @@ function createTriangles(p0, p1, p2, lineWidth, vertices) {
         vertices.push(subtractPoints(p1, t2));
         vertices.push(subtractPoints(p2, t2));
 
-    } else {
+    }
+    else
+    {
 
         vertices.push(addPoints(p0, t0));
         vertices.push(subtractPoints(p0, t0));
